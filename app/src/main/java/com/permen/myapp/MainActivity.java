65794a7ce3ebd124674b9da21db9a68a1c71aa6e
Łukasz Mijała <lukasz.mijala@gmail.com>
2023-11-08 13:38:05 +0100

    added new files

---
+++ app/src/main/java/com/permen/myapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.permen.myapp;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		WebView webView=(WebView) findViewById(R.id.mainwebView);
+		
+		webView.loadUrl("file:///android_asset/www/index.html");
+		webView.getSettings().setJavaScriptEnabled(true);
+    }
+}
